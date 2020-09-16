9771120f350a09702a5e7b6150c961c5551ca7aa
Brad Treloar <brad@allbizsupplies.biz>
2020-09-16 15:30:05 +0930

    cleanup

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -23,32 +23,4 @@ class UserTest extends TestCase
         $user->delete();
         $this->assertDeleted($user);
     }
-
-    public function testCreateUser()
-    {
-        $this->seed();
-        $user = factory(User::class)->make();
-
-        $request_data = [
-            "data" => [
-                "type" => "users",
-                "attributes" => [
-                    "name" => $user->name,
-                    "email" => $user->email,
-                    "password" => $user->password,
-                    "roles" => $user->roles,
-                ],
-            ],
-        ];
-
-        $response = $this->actingAs($user)
-            ->postJson("/api/v1/timesheets", $request_data, [
-                'Content-Type' => 'application/vnd.api+json',
-                'Accept' => 'application/vnd.api+json'
-            ]);
-
-        $response->assertStatus(201);
-
-        $this->assertDatabaseCount('users', 3);
-    }
 }
