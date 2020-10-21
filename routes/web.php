9b5f57017b62955023d2310348a64a70e4ecde83
Brad Treloar <brad@allbizsupplies.biz>
2020-10-21 13:12:08 +1030

    remove unused routes

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes([
-    'register' => false,
-]);
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::resource('users', 'UserController');
