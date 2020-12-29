5d83aa2cf8b746680ceabe7e1c97726715565653
Brad Treloar <brad@treloardigital.com.au>
2020-12-30 02:57:37 +1030

    update mail templates

--- resources/views/mail/layout.blade.php
+++ resources/views/mail/layout.blade.php
@@ -4,30 +4,23 @@
             <div style="background: #ffffff; padding: 1rem 0.5rem; width: 100%; max-width: 720px; margin: 0 auto;">
                 <div style="text-align:center; padding-bottom: 1rem; border-bottom: solid 1px #cccccc; margin-bottom: 1rem;">
                     <a style="display: inline-block;" href="{{ url('/') }}">
-                        <img style="display: block" src="{{ $message->embed(public_path() . "/images/logo.png") }} alt="{{ config('app.name', 'Laravel') }}"/>
+                        <img style="display: block" src="{{ $message->embed(public_path() . "/images/logo.png") }}" alt="{{ config('app.name', 'Laravel') }}"/>
                     </a>
                 </div>
                 <div>
-                <table style="margin: 0 auto; width: 100%; max-width: 720px;font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif">
-	                <tbody>
-		                <tr>
-			                <td>
-				                @yield('content')
-                            </td>
-                        </tr>
-                    </tbody>
+                    <table style="margin: 0 auto; width: 100%; max-width: 720px;font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif">
+                        <tbody>
+                            <tr>
+                                <td>
+                                    @yield('content')
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-            <div style="padding: 1rem 0.5rem; width: 100%; max-width: 720px; margin: 0 auto;">
-                <p>
-                    <strong>{{ config('app.name', 'Laravel') }}</strong>
-                </p>
-                <p>
-                    125 O'Sullivan Beach Road<br/>
-                    Lonsdale, South Australia
-                </p>
-                <p>P: 08 8326 2899<br/></p>
-                <p>E: timesheet@allbizsupplies.biz</p>
+            <div style="padding:1rem 0.5rem;width:100%;max-width: 720px;margin: 0 auto;font-weight:bold">
+                {{ config('app.name', 'Laravel') }}
             </div>
         </div>
     </body>
--- resources/views/mail/welcome.blade.php
+++ resources/views/mail/welcome.blade.php
@@ -1,18 +1,19 @@
 @extends('mail.layout')
 
 @section('content')
-<table style="width: 100%; min-width: 450px; border-collapse:collapse;">
+<table style="width: 100%;border-collapse:collapse;">
     <tbody>
         <tr>
             <td style="font-size:1.25rem;font-weight:bold;padding:1rem 0">Welcome</td>
+        </tr>
+        <tr>
             <td style="padding:1rem 0">
-                A user account has been created for you.
+                A user account has been created for you. Before you can log in, you need to set your secret password.
             </td>
+        </tr>
+        <tr>
             <td style="padding:1rem 0">
-                Before you can log in, you need to set your secret password.
-            </td>
-            <td style="padding:1rem 0">
-                <a style="padding:0.75rem 1rem;background-color:royalblue;color:white;" href="{{ $url }}">
+                <a style="padding:0.75rem 1rem;background-color:royalblue;color:white;text-decoration:none" href="{{ $url }}">
                     Click here to set your password
                 </a>
             </td>
